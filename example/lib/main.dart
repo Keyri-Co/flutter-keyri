d141e970486801f9f67dde4fbef6231507971b1d
Ruslan Gremechevsky <grem.rusl@gmail.com>
2022-07-21 17:38:39 +0300

    Update main file codestyle

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,15 +6,17 @@ void main() {
   runApp(const MyApp());
 }
 
+const String appKey = 'IT7VrTQ0r4InzsvCNJpRCRpi1qzfgpaj';
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Keyri SDK',
+        title: 'Keyri Example',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: const KeyriHomePage(title: 'Keyri SDK'));
+        home: const KeyriHomePage(title: 'Keyri Example'));
   }
 }
 
@@ -48,22 +50,17 @@ class _KeyriHomePageState extends State<KeyriHomePage> {
 
   void _easyKeyriAuth() async {
     await keyri
-        .easyKeyriAuth('IT7VrTQ0r4InzsvCNJpRCRpi1qzfgpaj', 'Some payload',
-            'Public user ID')
+        .easyKeyriAuth(appKey, 'Some payload', 'Public user ID')
         .then((authResult) => _onAuthResult(authResult))
-        .catchError((error, stackTrace) {
-      _onError(error);
-    });
+        .catchError((error, stackTrace) => _onError(error));
   }
 
   void _customUI() {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => const KeyriScannerAuthPage()));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const KeyriScannerAuthPage()));
   }
 
   void _onError(String message) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
   }
 
   void _onAuthResult(bool result) {
@@ -102,6 +99,27 @@ class _KeyriScannerAuthPageState extends State<KeyriScannerAuthPage> {
 
   Keyri keyri = Keyri();
 
+  void onMobileScannerDetect(Barcode barcode, MobileScannerArguments? args) {
+    if (barcode.rawValue == null) {
+      debugPrint('Failed to scan Barcode');
+      return;
+    }
+
+    final String? code = barcode.rawValue;
+    debugPrint('Scanned barcode: $code');
+
+    if (code == null) return;
+
+    var sessionId = Uri.dataFromString(code).queryParameters['sessionId'];
+
+    if (sessionId == null) return;
+
+    setState(() {
+      _isLoading = true;
+    });
+    _onReadSessionId(sessionId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -115,29 +133,7 @@ class _KeyriScannerAuthPageState extends State<KeyriScannerAuthPage> {
                         mainAxisSize: MainAxisSize.max,
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: const [CircularProgressIndicator()]))
-                : MobileScanner(
-                    allowDuplicates: false,
-                    onDetect: (barcode, args) {
-                      if (barcode.rawValue == null) {
-                        debugPrint('Failed to scan Barcode');
-                      } else {
-                        final String? code = barcode.rawValue;
-                        debugPrint('Scanned barcode: $code');
-
-                        if (code != null) {
-                          var sessionId = Uri.dataFromString(code)
-                              .queryParameters['sessionId'];
-
-                          if (sessionId != null) {
-                            setState(() {
-                              _isLoading = true;
-                            });
-
-                            _onReadSessionId(sessionId);
-                          }
-                        }
-                      }
-                    }),
+                : MobileScanner(allowDuplicates: false, onDetect: onMobileScannerDetect),
           )
         ],
       ),
@@ -146,22 +142,16 @@ class _KeyriScannerAuthPageState extends State<KeyriScannerAuthPage> {
 
   Future<void> _onReadSessionId(String sessionId) async {
     await keyri
-        .initiateQrSession(
-            'IT7VrTQ0r4InzsvCNJpRCRpi1qzfgpaj', sessionId, 'Public user ID')
+        .initiateQrSession(appKey, sessionId, 'Public user ID')
         .then((session) => keyri
-                .initializeDefaultScreen(sessionId, 'Some payload')
-                .then((authResult) => _onAuthResult(authResult))
-                .catchError((error, stackTrace) {
-              _onError(error.toString());
-            }))
-        .catchError((error, stackTrace) {
-      _onError(error.toString());
-    });
+            .initializeDefaultScreen(sessionId, 'Some payload')
+            .then((authResult) => _onAuthResult(authResult))
+            .catchError((error, stackTrace) => _onError(error.toString())))
+        .catchError((error, stackTrace) => _onError(error.toString()));
   }
 
   void _onError(String message) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
 
     setState(() {
       _isLoading = false;
