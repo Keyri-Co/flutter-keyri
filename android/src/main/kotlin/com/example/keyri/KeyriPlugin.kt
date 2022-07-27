9f87541f95b26b037303bddd12ece3ec67afcdb6
kuliahin.a <kuliahin.a@abcloudz.com>
2022-07-27 12:13:06 +0300

    Update Keyri version

--- android/src/main/kotlin/com/example/keyri/KeyriPlugin.kt
+++ android/src/main/kotlin/com/example/keyri/KeyriPlugin.kt
@@ -6,8 +6,8 @@ import android.app.Activity
 import android.content.Intent
 import com.google.gson.Gson
 import com.keyrico.keyrisdk.Keyri
+import com.keyrico.scanner.AuthWithScannerActivity
 import androidx.fragment.app.FragmentActivity
-import com.keyrico.keyrisdk.ui.auth.AuthWithScannerActivity
 import com.keyrico.keyrisdk.entity.session.Session
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -219,7 +219,7 @@ class KeyriPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
                 result.error("initializeDefaultScreen", "payload must not be null", null)
             } else {
                 (activity as? FragmentActivity)?.supportFragmentManager?.let { fm ->
-                    keyri.initializeDefaultScreen(fm, session, payload)
+                    keyri.initializeDefaultConfirmationScreen(fm, session, payload)
                         .onSuccess { isAuthenticated ->
                             result.success(isAuthenticated)
                         }.onFailure {
